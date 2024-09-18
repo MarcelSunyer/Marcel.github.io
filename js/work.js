a3bfeef334dee4c70d94bcb205eae039605e2ff3
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-09-18 20:43:28 +0200

    Update all projects

--- js/work.js
+++ js/work.js
@@ -1,7 +1,7 @@
 const works = [
     {   
         title_out : 'ClapCom Studios',
-        title: 'Alien Extraction',
+        title: '<strong>Alien Extraction',
         image: 'images/Alien_Extraction.png',
         description: '<b>Alien Extraction is a DIABLO-LIKE game themed in the ALIEN universe.</b><br><br>  Players will take on the role of an elite soldier sent to an alien-infested planet to clear it of all hostile forms. The action focuses on multi-level raids, where players will have to explore, fight against saga enemies, manage their resources carefully to survive and not die in their attempt to complete the mission and discover the truth.',
         additionalMedia: [
@@ -12,11 +12,12 @@ const works = [
     },
     {
         title_out : 'Custome Engine: Amarillo Engine',
-        title: 'Amarillo Engine',
-        image: 'images/Amarillo Logo.png',        description: 'Descripción del Trabajo 1',
+        title: '<strong>Amarillo Engine',
+        image: 'images/Amarillo Logo.png',        description: 'We were asked to make our own Game Engine for later use in Project III and add a high-level system to it. We decided to add a C#-like scripting system, because thats what most of the class already knew how to use.<h3>Key Features</h3> - Model Inport + Drag & Drio <br> - Texture Import <br> - GameOBject hierarchy `Component Pattern <br> - Unity-like camera controls <br> -Editor tools (main menu and windows) <br> - Transformation and Game Object hierarchy <br> - Custom file format <br> - Game viewport & editor viewport <br> - Scene serialization <br> - Play / Stop <br> - Camera component, mouse pick & frustrum culling <br> - Resource Managment <br> <h3> Scripting Capabilities</h3> Our scripting system is built on the foundation of C#, providing a familiar androbust language for game developers. You can create, modify, and manage scriptseffortlessly, thanks to the seamless integration of the ImGuiTextEditor. Thisuser-friendly interface makes coding a breeze and facilitates a smoothdevelopment experience.',
+
         additionalMedia: [
-            { type: 'image', src: 'images/Own_Engine.png'},
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
+            { type: 'image', src: 'images/Amarilloengine.png'},
+            { type: 'video', src: 'videos/Amarillo_Engine_Trailer.mp4' }
         ],
         link: 'https://marcelsunyer.github.io/AmarilloEngine/'
     },
@@ -26,76 +27,74 @@ const works = [
         image: 'images/nexus.png',
         description: 'In this RPG we will play a university student who, due to the temptation of using a new super powerful AI, ends up being absorbed by the computer. The student will appear in an unknown world where he will have to find his way out.',
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
+            { type: 'image', src: 'images/nexus.png' },
             { type: 'video', src: 'videos/Dragged_Off_Time_trailer.mp4' }
         ],
-        link: 'https://francesctr4.github.io/Nexus-Studios/'
+        link: 'https://ericlr.itch.io/dragged-off-time'
     },
     {
         title_out : 'Guerrilla War Tribute',
-        title: 'Guerrilla War',
+        title: '<strong>Guerrilla War',
         image: 'images/GuerrillaWar.png',
-        description: 'Descripción del Trabajo 1',
+        description: 'Guerrilla War, released in Japan as Guevara (ゲバラ), is an overhead run and gun game produced by SNK. Originally released for arcades in 1987 as a coin-operated arcade game, Guerrilla War was later ported to Amstrad CPC, Apple II, Commodore 64, NES, PlayStation Network, and ZX Spectrum.<br><br> Guerrilla War followed the adventures of two unnamed rebel commandos (Che Guevara and Fidel Castro in the Japanese version) as they raid an unnamed Caribbean Island in order to free it from the rule of an unnamed tyrannical dictator. Along the way the players vanquish hordes of enemy soldiers while attempting to rescue hostages (with large score reductions for any hostages killed in the crossfire), collecting weapons from troopers and operating tanks. <br><br>We completed this project in three months during our first year of university. It was our first group assignment, and we worked together as a team of five.',
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
+            { type: 'image', src: 'images/guerrilla.png' },
+            { type: 'video', src: 'videos/TRIBUTE_GUERRILLA_WAR.mp4' }
         ],
-        link: 'https://github.com/ericlr1/Proyecto_1_Guerrilla-War'
+        link: 'https://ericlr.itch.io/guerrilla-war'
     },
     {
         title_out : 'Easing and Splines Tool for C++',
-        title: 'Easing and Splines for UI Animations',
+        title: '<strong>Easing and Splines for UI Animations',
         image: 'images/Splines.png',
-        description: 'Descripción del Trabajo 1',
+        description: 'This program shows differents examples of how Easing can be applied in many areas of a project in order to give life to movements and actions and overall enhance UX. <br> <h3>Key Features</h3> Here you will se a small environment with grass, a sign and a tree. Move inside the sign to move the camera towards the tree. Step out to make the camera return. <br><br> <h3> Controls</h3> Use W A S D to move the character to get the animation on different areas.<br> Press P, Pause menu will fall with an animation.<br> Press I, will appear thhe inventory from the bottom of the srean with an animation.',
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
         ],
         link: 'https://github.com/MarcelSunyer/Easing-and-Splines-for-UI-Animations'
     },
     {
         title_out : 'VR: Escape Room',
-        title: 'Escape Room',
+        title: '<strong>Escape Room',
         image: 'images/Escape_Room.png',
-        description: 'Descripción del Trabajo 1',
+        description: 'Immerse yourself in a thrilling virtual escape room where your hands are the key! Using advanced hand-tracking technology, solve intricate puzzles, interact with objects, and unlock secrets with intuitive gestures. Feel the tension rise as the clock ticks down—will you escape in time?',
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
+            { type: 'image', src: 'images/Vr.png '},
+            { type: 'video', src: 'videos/Escape_Room_VR_Trailer.mp4' }
         ],
         link: 'https://github.com/francesctr4/EscapeRoomVR'
     },
     {
         title_out : 'Advanced Game AI Scene',
-        title: 'Advanced-Game-AI-Scene',
+        title: '<strong>Advanced-Game-AI-Scene',
         image: 'images/Unity_ia.png',
-        description: 'Descripción del Trabajo 1',
+        description: 'This repository houses a project dedicated to creating a dynamic and free scene populated with numerous agents, each following specific behavioral constraints. The primary goal is to implement various artificial intelligence techniques, drawing inspiration from the methodologies covered in relevant course exercises. <br> <h3>Project Highlights</h3> <strong>Diverse AI Techniques</strong>: The project incorporates a multitude of AI techniques explored during the course, showcasing a comprehensive understanding of the subject matter.<br> <strong>Behavior Bricks Integration:</strong> Significantly, the behavior of two agents within the scene is exclusively modeled using Behavior Bricks, providing a concrete demonstration of this behavioral design framework.<br> <strong>Player Exclusion:</strong> Notably, the absence of a player entity is a deliberate design choice within the project.' ,
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
+            { type: 'image', src: 'images/Ia.png' },
+            { type: 'image', src: 'images/Ia_2.png' },
         ],
-        link: 'https://github.com/francesctr4/EscapeRoomVR'
+        link: 'https://github.com/MarcelSunyer/Advanced-Game-AI-Scene'
     },
     {
         title_out : '6a CITM GJ: Bat Reverb',
-        title: 'Bat Reverb',
+        title: '<strong>Bat Reverb',
         image: 'images/bat.png',
-        description: 'Descripción del Trabajo 1',
+        description: 'The game begins with a sleeping bat, who is startled awake by the echoing sound of what seems to be his mother. He gets up and begins to follow the sound.As he progresses through the world, he collects tools and amulets that explain the context of the world you find yourself in. But whats that sound really mean? We completed this project in five days during 6ª CITM Game Jam. It was our first game jam, and we worked together as a team of five. The theme of the Game Jam was "Echo".',
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
+            { type: 'image', src: 'images/bat.png' },
+            { type: 'video', src: 'videos/6a_CITM_Game_Jam_Bat.mp4' }
         ],
-        link: 'https://github.com/francesctr4/EscapeRoomVR'
+        link: 'https://ericlr.itch.io/bat-reverb'
     },
     {
         title_out : '7a CITM GJ: Nightmeres',
-        title: 'Nightmeres of the night',
+        title: '<strong>Nightmeres of the night',
         image: 'images/nigthmares.png',
-        description: 'Descripción del Trabajo 1',
+        description: '"Nightmares of the Night" is a chilling horror game where players navigate through their own house armed with a dart gun, battling a variety of nightmares and monsters lurking in the darkness. As players progress through each room, they confront terrifying creatures while inching closer to their ultimate destination: their parents bedroom. We completed this project in five days during 7ª CITM Game Jam. It was our second game jam, and we worked together as a team of five. The theme of the Game Jam was "Dreams and Nightmares".',
         additionalMedia: [
-            { type: 'image', src: 'images/Alien_Extraction.jpg' },
-            { type: 'video', src: 'videos/Dragged_Off_Time.mp4' }
+            { type: 'image', src: 'images/nightmers.png' },
+            { type: 'video', src: 'videos/Nightmares_Of_The_Night _Trailer.mp4' }
         ],
-        link: 'https://github.com/francesctr4/EscapeRoomVR'
+        link: 'https://ericlr.itch.io/nightmares-of-the-night'
     },
 ];
 
